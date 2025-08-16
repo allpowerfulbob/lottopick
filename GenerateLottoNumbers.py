86d7b1d79362ede15ae6c5bbfb2a32993de9f827
Bob <bob.sheets76@gmail.com>
2025-08-16 15:54:29 -0600

    Still trying to debug attribute errors

--- GenerateLottoNumbers.py
+++ GenerateLottoNumbers.py
@@ -1,9 +1,15 @@
 # Import the random module
 import random
 
+class LottoNumberGenerator:
+        def __init__ (self, co_generator, pb_generator, mm_generator):
+            co_generator = generate_lotto_numbers_colorado_lotto
+            pb_generator = generate_lotto_numbers_power_ball
+            mm_generator = generate_lotto_numbers_mega_millions
+
 def generate_lotto_numbers_colorado_lotto(self):
-    col_main_numbers = random.sample(range(1, 40), 5)
-    return col_main_numbers
+    self.col_main_numbers = random.sample(range(1, 40), 5)
+    return self.col_main_numbers
 
 def generate_lotto_numbers_power_ball(self):
     pb_main_numbers = random.sample(range(1, 70), 5)
@@ -15,8 +21,3 @@ def generate_lotto_numbers_mega_millions(self):
     mm_mega_ball = random.randint(1, 24)
     return mm_main_numbers, mm_mega_ball
 
-class LottoNumberGenerator:
-        def __init__ (self, co_generator, pb_generator, mm_generator):
-            self.co_generator = generate_lotto_numbers_colorado_lotto
-            self.pb_generator = generate_lotto_numbers_power_ball
-            self.mm_generator = generate_lotto_numbers_mega_millions
--- LottoPickMain.py
+++ LottoPickMain.py
@@ -1,20 +1,22 @@
 # Copyright (c) Bob Sheets 2025 all rights reserved
 # Powerball and Megamillions propety of their respective owners
 # Import generate lotto numbers module
-import GenerateLottoNumbers
+from GenerateLottoNumbers import LottoNumberGenerator
 import re
+import unittest
 
 def main():
     
 
-    choice = input("Choose a lottery type (Colorado Lotto, Power Ball, Mega Millions): ")
-    if re.match("Colorado Lotto", choice, re.IGNORECASE):
-        print("Here are your Colorado Lotto Numbers: " + str(GenerateLottoNumbers.co_generator()))
+    choice = input("Choose a lottery type (Colorado, Power Ball, Mega Millions): ")
+    if re.match("Colorado", choice, re.IGNORECASE):
+        print("Here are your Colorado Lottery Numbers: " + str(LottoNumberGenerator.co_generator()))
     elif re.match("Power Ball", choice, re.IGNORECASE):
-        print("Here are your Power Ball Numbers: " + str(GenerateLottoNumbers.pb_generator()))
+        print("Here are your Power Ball Numbers: " + str(LottoNumberGenerator.pb_generator()))
     elif re.match("Mega Millions", choice, re.IGNORECASE):
-        print("Here are your Mega Millions Numbers: " + str(GenerateLottoNumbers.mm_generator()))
+        print("Here are your Mega Millions Numbers: " + str(LottoNumberGenerator.mm_generator()))
     else: print("Invalid choice. Please try again.")
+    
 
 if __name__ == '__main__':
     main()
