62a69ad5ea400e1e054ea0c98b005847dafeed7c
Bob <bob.sheets76@gmail.com>
2025-08-18 09:18:55 -0600

    Still troubleshooting no attribute error

--- GenerateLottoNumbers.py
+++ GenerateLottoNumbers.py
@@ -7,17 +7,17 @@ class LottoNumberGenerator:
             pb_generator = generate_lotto_numbers_power_ball
             mm_generator = generate_lotto_numbers_mega_millions
 
-def generate_lotto_numbers_colorado_lotto(self):
-    self.col_main_numbers = random.sample(range(1, 40), 5)
-    return self.col_main_numbers
+            def generate_lotto_numbers_colorado_lotto(self):
+                self.col_main_numbers = random.sample(range(1, 40), 5)
+                return self.col_main_numbers
 
-def generate_lotto_numbers_power_ball(self):
-    pb_main_numbers = random.sample(range(1, 70), 5)
-    power_ball = random.randint(1, 26)
-    return pb_main_numbers, power_ball
+            def generate_lotto_numbers_power_ball(self):
+                pb_main_numbers = random.sample(range(1, 70), 5)
+                power_ball = random.randint(1, 26)
+                return pb_main_numbers, power_ball
 
-def generate_lotto_numbers_mega_millions(self):
-    mm_main_numbers = random.sample(range(1, 70), 5)
-    mm_mega_ball = random.randint(1, 24)
-    return mm_main_numbers, mm_mega_ball
+            def generate_lotto_numbers_mega_millions(self):
+                mm_main_numbers = random.sample(range(1, 70), 5)
+                mm_mega_ball = random.randint(1, 24)
+                return mm_main_numbers, mm_mega_ball
 
--- LottoPickMain.py
+++ LottoPickMain.py
@@ -1,7 +1,7 @@
 # Copyright (c) Bob Sheets 2025 all rights reserved
 # Powerball and Megamillions propety of their respective owners
 # Import generate lotto numbers module
-from GenerateLottoNumbers import LottoNumberGenerator
+import GenerateLottoNumbers
 import re
 import unittest
 
@@ -10,13 +10,15 @@ def main():
 
     choice = input("Choose a lottery type (Colorado, Power Ball, Mega Millions): ")
     if re.match("Colorado", choice, re.IGNORECASE):
-        print("Here are your Colorado Lottery Numbers: " + str(LottoNumberGenerator.co_generator()))
+        print("Here are your Colorado Lottery Numbers: " + str(GenerateLottoNumbers.generate_lotto_numbers_colorado_lotto()))
     elif re.match("Power Ball", choice, re.IGNORECASE):
-        print("Here are your Power Ball Numbers: " + str(LottoNumberGenerator.pb_generator()))
+        print("Here are your Power Ball Numbers: " + str(GenerateLottoNumbers.generate_lotto_numbers_power_ball()))
     elif re.match("Mega Millions", choice, re.IGNORECASE):
-        print("Here are your Mega Millions Numbers: " + str(LottoNumberGenerator.mm_generator()))
+        print("Here are your Mega Millions Numbers: " + str(GenerateLottoNumbers.generate_lotto_numbers_mega_millions()))
     else: print("Invalid choice. Please try again.")
-    
+
+
+
 
 if __name__ == '__main__':
     main()
