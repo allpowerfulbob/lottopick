c3b8aa1668f6746a1e5f3fe348b4d50587677121
Nicholas Sawlidi <nicholas.sawlidi@perforce.com>
2025-08-18 09:42:15 -0600

    Removing the class to simplify the code

--- GenerateLottoNumbers.py
+++ GenerateLottoNumbers.py
@@ -1,23 +1,16 @@
 # Import the random module
 import random
 
-class LottoNumberGenerator:
-        def __init__ (self, co_generator, pb_generator, mm_generator):
-            co_generator = generate_lotto_numbers_colorado_lotto
-            pb_generator = generate_lotto_numbers_power_ball
-            mm_generator = generate_lotto_numbers_mega_millions
+def generate_lotto_numbers_colorado_lotto():
+     return random.sample(range(1, 40), 5)
 
-            def generate_lotto_numbers_colorado_lotto(self):
-                self.col_main_numbers = random.sample(range(1, 40), 5)
-                return self.col_main_numbers
+def generate_lotto_numbers_power_ball():
+    pb_main_numbers = random.sample(range(1, 70), 5)
+    power_ball = random.randint(1, 26)
+    return pb_main_numbers, power_ball
 
-            def generate_lotto_numbers_power_ball(self):
-                pb_main_numbers = random.sample(range(1, 70), 5)
-                power_ball = random.randint(1, 26)
-                return pb_main_numbers, power_ball
-
-            def generate_lotto_numbers_mega_millions(self):
-                mm_main_numbers = random.sample(range(1, 70), 5)
-                mm_mega_ball = random.randint(1, 24)
-                return mm_main_numbers, mm_mega_ball
+def generate_lotto_numbers_mega_millions():
+    mm_main_numbers = random.sample(range(1, 70), 5)
+    mm_mega_ball = random.randint(1, 24)
+    return mm_main_numbers, mm_mega_ball
 
--- LottoPickMain.py
+++ LottoPickMain.py
@@ -6,22 +6,18 @@ import re
 import unittest
 
 def main():
-    
 
     choice = input("Choose a lottery type (Colorado, Power Ball, Mega Millions): ")
     if re.match("Colorado", choice, re.IGNORECASE):
-        print("Here are your Colorado Lottery Numbers: " + 
-              str(GenerateLottoNumbers.co_generator()))
+        print("Here are your Colorado Lottery Numbers: " +
+              str(GenerateLottoNumbers.generate_lotto_numbers_colorado_lotto()))
     elif re.match("Power Ball", choice, re.IGNORECASE):
-        print("Here are your Power Ball Numbers: " + 
-              str(GenerateLottoNumbers.pb_generator()))
+        print("Here are your Power Ball Numbers: " +
+              str(GenerateLottoNumbers.generate_lotto_numbers_power_ball()))
     elif re.match("Mega Millions", choice, re.IGNORECASE):
-        print("Here are your Mega Millions Numbers: " + 
-              str(GenerateLottoNumbers.mm_generator()))
+        print("Here are your Mega Millions Numbers: " +
+              str(GenerateLottoNumbers.generate_lotto_numbers_mega_millions()))
     else: print("Invalid choice. Please try again.")
-
-
-
 
 if __name__ == '__main__':
     main()
