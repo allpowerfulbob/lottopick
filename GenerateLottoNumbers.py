6280475a90f1d49882f1095529561ec3bc127af3
Bob <bob.sheets76@gmail.com>
2025-08-18 10:04:58 -0600

    added a sort to the numbers

--- GenerateLottoNumbers.py
+++ GenerateLottoNumbers.py
@@ -2,15 +2,15 @@
 import random
 
 def generate_lotto_numbers_colorado_lotto():
-     return random.sample(range(1, 40), 5)
+     return sorted(random.sample(range(1, 40), 5))
 
 def generate_lotto_numbers_power_ball():
     pb_main_numbers = random.sample(range(1, 70), 5)
     power_ball = random.randint(1, 26)
-    return pb_main_numbers, power_ball
+    return sorted(pb_main_numbers), power_ball
 
 def generate_lotto_numbers_mega_millions():
     mm_main_numbers = random.sample(range(1, 70), 5)
     mm_mega_ball = random.randint(1, 24)
-    return mm_main_numbers, mm_mega_ball
+    return sorted(mm_main_numbers), mm_mega_ball
 
