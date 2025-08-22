79038b3dbd4b43f8d2effdb1f9643159a9583512
Bob <bob.sheets76@gmail.com>
2025-08-22 13:55:04 -0600

    trying to correct skiptest

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -7,19 +7,22 @@ import LottoPickMain
 class TestLottoPick(unittest.TestCase):
     
     def test_lotto_pick_colorado(self):
-        self.skiptest("power ball", "mega millions")
-        with patch('builtins.input', side_effect=['Colorado', 1]):
+        @unittest.skipIf("power ball", "mega millions", re.IGNORECASE)
+        @patch('builtins.input', side_effect=['Colorado', 1])
+        def test_lotto_pick_colorado(self):
             result = LottoPickMain.main()
             self.assertEqual(result("colorado", re.IGNORECASE), True)
 
     def test_lotto_pick_power_ball(self):
-        self.skiptest("colorado", "mega millions")
-        with patch('builtins.input', side_effect=['Power Ball', 1]):
+        @unittest.skipIf("colorado", "mega millions", re.IGNORECASE)
+        @patch('builtins.input', side_effect=['Power Ball', 1])
+        def test_lotto_pick_power_ball(self):
             result = LottoPickMain.main()
             self.assertEqual(result("power ball", re.IGNORECASE), True)
 
     def test_lotto_pick_mega_millions(self):
-        self.skipTest("colorado", "power ball")
-        with patch('builtins.input', side_effect=['Mega Millions', 1]):
+        @unittest.skipif("colorado", "power ball", re.IGNORECASE)
+        @patch('builtins.input', side_effect=['Mega Millions', 1])
+        def test_lotto_pick_mega_millions(self):
             result = LottoPickMain.main()
             self.assertEqual(result("mega millions", re.IGNORECASE), True)
