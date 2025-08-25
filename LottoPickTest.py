3ab537327f88ea66f7009cc2c71c9d13de73ff75
Bob <bob.sheets76@gmail.com>
2025-08-25 10:39:39 -0600

    Corrected selections for skipIf decorators

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,8 +6,7 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    @unittest.skipIf("power ball or mega millions", re.IGNORECASE,
-                         "Testing Colorado Input")
+    @unittest.skipIf("power ball or mega millions", "Testing Colorado Input")
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_colorado(self, mock_input, choice, tickets):
         
@@ -16,14 +15,14 @@ class TestLottoPick(unittest.TestCase):
         result = LottoPickMain.main()
         self.assertEqual(result("colorado", re.IGNORECASE), True)
 
-    @unittest.skipIf("colorado", "mega millions", re.IGNORECASE)
+    @unittest.skipIf("colorado or mega millions", "Testing Power Ball"
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_power_ball(self, mock_input, choice, tickets):
         
         result = LottoPickMain.main()
         self.assertEqual(result("power ball", re.IGNORECASE), True)
 
-    @unittest.skipif("colorado", "power ball", re.IGNORECASE)
+    @unittest.skipif("colorado or power ball", "Testing Mega Millions")
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_mega_millions(self, mock_input, choice, ticktets):
         
