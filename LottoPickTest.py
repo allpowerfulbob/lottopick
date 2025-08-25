c865663a6ceead46603273162c9bf68e606aeec6
Bob <bob.sheets76@gmail.com>
2025-08-25 13:42:10 -0600

    convert return_value to str

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,24 +6,23 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    @patch('builtins.input', return_value=['choice', 'tickets'])
+    @patch('builtins.input', return_value=str('choice', 'tickets'))
     def test_lotto_pick_colorado(self, mock_input):
-        
-        choice = mock_input('colorado')
-        tickets = mock_input(1)
+        choice = str(mock_input('colorado'))
+        tickets = str(mock_input('1'))
         result = LottoPickMain.main()
-        self.assertEqual(result("colorado", re.IGNORECASE), True)
+        self.assertEqual(result("colorado"), True)
 
-    @patch('builtins.input', return_value=['choice', 'tickets'])
+    @patch('builtins.input', return_value=str('choice', 'tickets'))
     def test_lotto_pick_power_ball(self, mock_input):
         choice = mock_input('power ball')
-        tickets = mock_input(1)
+        tickets = mock_input('1')
         result = LottoPickMain.main()
-        self.assertEqual(result("power ball", re.IGNORECASE), True)
+        self.assertEqual(result("power ball"), True)
 
-    @patch('builtins.input', return_value=['choice', 'tickets'])
+    @patch('builtins.input', return_value=str('choice', 'tickets'))
     def test_lotto_pick_mega_millions(self, mock_input):
         choice = mock_input('mega millions')
-        tickets = mock_input(1)
+        tickets = mock_input('1')
         result = LottoPickMain.main()
-        self.assertEqual(result("mega millions", re.IGNORECASE), True)
+        self.assertEqual(result("mega millions"), True)
