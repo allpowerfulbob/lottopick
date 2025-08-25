93052447a3181781d93691a006685cdc7fba3d9e
Bob <bob.sheets76@gmail.com>
2025-08-25 10:51:00 -0600

    removed skipIf statements

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,7 +6,6 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    @unittest.skipIf("power ball or mega millions", "Testing Colorado Input")
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_colorado(self, mock_input, choice, tickets):
         
@@ -15,14 +14,12 @@ class TestLottoPick(unittest.TestCase):
         result = LottoPickMain.main()
         self.assertEqual(result("colorado", re.IGNORECASE), True)
 
-    @unittest.skipIf("colorado or mega millions", "Testing Power Ball")
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_power_ball(self, mock_input, choice, tickets):
         
         result = LottoPickMain.main()
         self.assertEqual(result("power ball", re.IGNORECASE), True)
 
-    @unittest.skipIf("colorado or power ball", "Testing Mega Millions")
     @patch('builtins.input', return_value=['choice', 'tickets'])
     def test_lotto_pick_mega_millions(self, mock_input, choice, ticktets):
         
