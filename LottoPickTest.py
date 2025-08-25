be6c2d64cd8509e9704e0bd9cd8871fabee18808
Bob <bob.sheets76@gmail.com>
2025-08-25 13:46:13 -0600

    changing to str didn't work, changed back several changes

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,21 +6,21 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    @patch('builtins.input', return_value=str('choice', 'tickets'))
+    @patch('builtins.input', return_value=('choice', 'tickets'))
     def test_lotto_pick_colorado(self, mock_input):
-        choice = str(mock_input('colorado'))
-        tickets = str(mock_input('1'))
+        choice = (mock_input('colorado'))
+        tickets = (mock_input('1'))
         result = LottoPickMain.main()
         self.assertEqual(result("colorado"), True)
 
-    @patch('builtins.input', return_value=str('choice', 'tickets'))
+    @patch('builtins.input', return_value=('choice', 'tickets'))
     def test_lotto_pick_power_ball(self, mock_input):
         choice = mock_input('power ball')
         tickets = mock_input('1')
         result = LottoPickMain.main()
         self.assertEqual(result("power ball"), True)
 
-    @patch('builtins.input', return_value=str('choice', 'tickets'))
+    @patch('builtins.input', return_value=('choice', 'tickets'))
     def test_lotto_pick_mega_millions(self, mock_input):
         choice = mock_input('mega millions')
         tickets = mock_input('1')
