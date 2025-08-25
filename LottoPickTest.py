8bcb95cacc25ebf7e9cdce3c7350d1d0c60dc766
Bob <bob.sheets76@gmail.com>
2025-08-25 11:01:33 -0600

    Forgot to add arguments for powerball and mega millions. Deleted unnecesary code in def

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -7,7 +7,7 @@ import LottoPickMain
 class TestLottoPick(unittest.TestCase):
     
     @patch('builtins.input', return_value=['choice', 'tickets'])
-    def test_lotto_pick_colorado(self, mock_input, choice, tickets):
+    def test_lotto_pick_colorado(self, mock_input):
         
         choice = mock_input('colorado')
         tickets = mock_input(1)
@@ -15,13 +15,15 @@ class TestLottoPick(unittest.TestCase):
         self.assertEqual(result("colorado", re.IGNORECASE), True)
 
     @patch('builtins.input', return_value=['choice', 'tickets'])
-    def test_lotto_pick_power_ball(self, mock_input, choice, tickets):
-        
+    def test_lotto_pick_power_ball(self, mock_input):
+        choice = mock_input('power ball')
+        tickets = mock_input(1)
         result = LottoPickMain.main()
         self.assertEqual(result("power ball", re.IGNORECASE), True)
 
     @patch('builtins.input', return_value=['choice', 'tickets'])
-    def test_lotto_pick_mega_millions(self, mock_input, choice, ticktets):
-        
+    def test_lotto_pick_mega_millions(self, mock_input):
+        choice = mock_input('mega millions')
+        tickets = mock_input(1)
         result = LottoPickMain.main()
         self.assertEqual(result("mega millions", re.IGNORECASE), True)
