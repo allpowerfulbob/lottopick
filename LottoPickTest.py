f496f7a89150cd73326e256a16646d6aabba4908
Bob <bob.sheets76@gmail.com>
2025-08-25 10:36:06 -0600

    changed location of decorators

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,23 +6,26 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    def test_lotto_pick_colorado(self, mock_input, choice, tickets):
-        @unittest.skipIf(choice == "power ball or mega millions", re.IGNORECASE,
+    @unittest.skipIf(choice == "power ball or mega millions", re.IGNORECASE,
                          "Testing Colorado Input")
-        @patch('builtins.input', return_value=['choice', 'tickets']):
+    @patch('builtins.input', return_value=['choice', 'tickets'])
+    def test_lotto_pick_colorado(self, mock_input, choice, tickets):
+        
         choice = mock_input('colorado')
         tickets = mock_input(1)
         result = LottoPickMain.main()
         self.assertEqual(result("colorado", re.IGNORECASE), True)
 
-    def test_lotto_pick_power_ball(self, mock_input):
-        @unittest.skipIf("colorado", "mega millions", re.IGNORECASE)
-        @patch('builtins.input', return_value=['choice', 'tickets']):
+    @unittest.skipIf("colorado", "mega millions", re.IGNORECASE)
+    @patch('builtins.input', return_value=['choice', 'tickets'])
+    def test_lotto_pick_power_ball(self, mock_input, choice, tickets):
+        
         result = LottoPickMain.main()
         self.assertEqual(result("power ball", re.IGNORECASE), True)
 
-    def test_lotto_pick_mega_millions(self, mock_input):
-        @unittest.skipif("colorado", "power ball", re.IGNORECASE)
-        @patch('builtins.input', return_value=['choice', 'tickets']):
+    @unittest.skipif("colorado", "power ball", re.IGNORECASE)
+    @patch('builtins.input', return_value=['choice', 'tickets'])
+    def test_lotto_pick_mega_millions(self, mock_input, choice, ticktets):
+        
         result = LottoPickMain.main()
         self.assertEqual(result("mega millions", re.IGNORECASE), True)
