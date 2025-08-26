98774a1274803971e736165b3868d16596997ed2
Bob <bob.sheets76@gmail.com>
2025-08-26 14:06:36 -0600

    still troubleshooting failures and error messages

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -3,23 +3,21 @@ import unittest
 from unittest.mock import patch
 import re
 import LottoPickMain
+import GenerateLottoNumbers
 
 class TestLottoPick(unittest.TestCase):
     
-    @patch('builtins.input', return_value=('colorado', '1'))
-    def test_lotto_pick_colorado(self, return_value):
-        result = LottoPickMain.main()
-        self.assertEqual(result("colorado"), True)
+    @patch('builtins.input', side_effect=('colorado', '1'))
+    def test_lotto_pick_colorado(self, mock_input):
+        self.assertEqual("colorado", GenerateLottoNumbers.generate_lotto_numbers_colorado_lotto)
 
-    @patch('builtins.input', return_value=('power ball', '1'))
-    def test_lotto_pick_power_ball(self, return_value):
-        result = LottoPickMain.main()
-        self.assertEqual(result("power ball"), True)
+    @patch('builtins.input', side_effect=('power ball', '1'))
+    def test_lotto_pick_power_ball(self, mock_input):
+        self.assertEqual("power ball")
 
-    @patch('builtins.input', return_value=('mega millions', '1'))
-    def test_lotto_pick_mega_millions(self, return_value):
-        result = LottoPickMain.main()
-        self.assertEqual(result("mega millions"), True)
+    @patch('builtins.input', side_effect=('mega millions', '1'))
+    def test_lotto_pick_mega_millions(self, mock_input):
+        self.assertEqual("mega millions")
 
 if __name__ == '__main__':
     unittest.main()
