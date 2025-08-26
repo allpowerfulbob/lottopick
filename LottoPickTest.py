33725394122c595edcb75356246e88dcde7d37ec
Bob <bob.sheets76@gmail.com>
2025-08-26 13:25:43 -0600

    Still chasing various errors

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -6,23 +6,20 @@ import LottoPickMain
 
 class TestLottoPick(unittest.TestCase):
     
-    @patch('builtins.input', return_value=('choice', 'tickets'))
-    def test_lotto_pick_colorado(self, mock_input):
-        choice = (mock_input('colorado'))
-        tickets = (mock_input('1'))
+    @patch('builtins.input', return_value=('colorado', '1'))
+    def test_lotto_pick_colorado(self):
+        
         result = LottoPickMain.main()
         self.assertEqual(result("colorado"), True)
 
-    @patch('builtins.input', return_value=('choice', 'tickets'))
-    def test_lotto_pick_power_ball(self, mock_input):
-        choice = mock_input('power ball')
-        tickets = mock_input('1')
+    @patch('builtins.input', return_value=('power ball', '1'))
+    def test_lotto_pick_power_ball(self):
+        
         result = LottoPickMain.main()
         self.assertEqual(result("power ball"), True)
 
-    @patch('builtins.input', return_value=('choice', 'tickets'))
+    @patch('builtins.input', return_value=('mega millions', '1'))
     def test_lotto_pick_mega_millions(self, mock_input):
-        choice = mock_input('mega millions')
-        tickets = mock_input('1')
+        
         result = LottoPickMain.main()
         self.assertEqual(result("mega millions"), True)
