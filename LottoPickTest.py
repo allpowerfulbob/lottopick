bc12bf5f6ebc1f82d5aff72f9eed5e5c0ecc2ffe
Bob <bob.sheets76@gmail.com>
2025-09-12 10:07:48 -0600

    adjusting tests to include individual outputs

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -10,7 +10,7 @@ class TestLottoPick(unittest.TestCase):
     @patch('builtins.input', side_effect=('colorado', '1'))
     def test_lotto_pick_colorado(self, mock_input):
         random_numbers_colorado = GenerateLottoNumbers.generate_lotto_numbers_colorado_lotto
-        self.assertEqual("colorado", "1")
+        self.assertEqual("colorado", "GenerateLottoNumbers.generate_lotto_numbers_colorado_lotto")
         for number in random_numbers_colorado:
             self.assertLess(5, 40)
 
@@ -18,14 +18,14 @@ class TestLottoPick(unittest.TestCase):
     @patch('builtins.input', side_effect=('power ball', '1'))
     def test_lotto_pick_power_ball(self, mock_input):
         random_numbers_power_ball = GenerateLottoNumbers.generate_lotto_numbers_power_ball
-        self.assertEqual("power ball", "1")
+        self.assertEqual("power ball", "GenerateLottoNumbers.generate_lotto_numbers_power_ball")
         for number in random_numbers_power_ball:
             self.assertLess(5, 40)
 
     @patch('builtins.input', side_effect=('mega millions', '1'))
     def test_lotto_pick_mega_millions(self, mock_input):
         random_numbers_mega_millions = GenerateLottoNumbers.generate_lotto_numbers_mega_millions
-        self.assertEqual("mega millions", "1")
+        self.assertEqual("mega millions", "GenerateLottoNumbers.generate_lotto_numbers_mega_millions")
         for number in random_numbers_mega_millions:
             self.assertLess(5, 40)
 
