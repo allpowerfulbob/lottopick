1469d83b95a90d5d20ee695509a32c28116d8e86
Bob <bob.sheets76@gmail.com>
2025-09-15 10:57:07 -0600

    Added comments for tests

--- LottoPickTest.py
+++ LottoPickTest.py
@@ -1,3 +1,5 @@
+# Copyright (c) Bob Sheets 2025 all rights reserved
+# Powerball and Megamillions propety of their respective owners
 # Import required modules
 import unittest
 import random
@@ -6,11 +8,14 @@ import re
 import LottoPickMain
 import GenerateLottoNumbers
 
+# Create the test class
 class TestLottoPick(unittest.TestCase):
 
+    # Create the rancom seed for the test
     def setUp(self):
         random.seed(42)
     
+    # Test for the Colorado Lottery pick
     @patch('builtins.input', side_effect=('colorado', '1'))
     def test_lotto_pick_colorado(self, mock_input):
         random_numbers_colorado = random.random
@@ -20,6 +25,7 @@ class TestLottoPick(unittest.TestCase):
             self.assertLess(5, 40)
 
 
+    # Test for the Power Ball Lottery Pick
     @patch('builtins.input', side_effect=('power ball', '1'))
     def test_lotto_pick_power_ball(self, mock_input):
         random_numbers_power_ball = GenerateLottoNumbers.generate_lotto_numbers_power_ball
@@ -27,6 +33,7 @@ class TestLottoPick(unittest.TestCase):
         for number in random_numbers_power_ball:
             self.assertLess(5, 40)
 
+    # Test for the Mega Millions Lottery Pick
     @patch('builtins.input', side_effect=('mega millions', '1'))
     def test_lotto_pick_mega_millions(self, mock_input):
         random_numbers_mega_millions = GenerateLottoNumbers.generate_lotto_numbers_mega_millions
